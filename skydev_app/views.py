e73fed99143a28d805c4f5848df3b93d898370cf
Saps <svsapegin@mail.ru>
2025-09-03 17:37:34 +0300

    first commit

---
+++ skydev_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Domain
+from .serializers import DomainSerializer
+
+
+
+class DomainViewSet(viewsets.ModelViewSet):
+    """Представление для профилей пользователей"""
+    queryset = Domain.objects.all()
+    serializer_class = DomainSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
