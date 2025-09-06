2789a1dc787744f57ea3c8958f22be3c40bab6a3
aelitta <a.berdnikova@gmail.com>
2025-09-07 00:40:45 +0300

    Update extract_keywords.py
    Добавлена функция Парсинга из таблицы в docx

--- extract_keywords.py
+++ extract_keywords.py
@@ -36,6 +36,32 @@ def extract_docx_file(file_path):
 
     return text_content
 
+def parse_table_from_docx(file_path):
+    """Парсит таблицу из DOCX файла"""
+    doc = Document(file_path)
+    table = doc.tables[0]
+
+    data = []
+    
+    keys = None
+    for i, row in enumerate(table.rows):
+        text = (cell.text for cell in row.cells)
+    
+        # Establish the mapping based on the first row
+        # headers; these will become the keys of our dictionary
+        if i == 0:
+            keys = tuple(text)
+            continue
+    
+        # Construct a dictionary for this row, mapping
+        # keys to values for this row
+        row_data = dict(zip(keys, text))
+        data.append(row_data)
+    
+    return data
+
+
+
 file_path = '/Users/aelitta/Documents/salute-speech/AI HR/Описание бизнес аналитик.docx'
 
 
